56ecc819ce41c18f560b2480c5e8889b94ab3044
bgunwani <bhawna.gunwani@gmail.com>
2021-09-17 13:00:03 +0530

    CSharp Fundamentals Commit

---
+++ DayThree-CSharpFundamentals/CSharp_Intermediate/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CSharp_Intermediate
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			System.Console.WriteLine("Hello Everyone");
+			System.Console.WriteLine("Lets Learn C#!!");
+		}
+	}
+}
